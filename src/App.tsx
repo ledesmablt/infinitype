55f08c7d80061d36a4b3ca0c79943ae70f117e21
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-05 19:58:52 +0800

    added header, main, footer components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 import AppContext from './context';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 const defaultContext = {
   theme: 'dark',
@@ -12,20 +15,9 @@ function App() {
   return (
     <div className="App">
       <AppContext.Provider value={defaultContext}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Main />
+        <Footer />
       </AppContext.Provider>
     </div>
   );
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Main.css';
+
+function Main() {
+  return (
+    <main>Main</main>
+  )
+}
+
+export default Main;
