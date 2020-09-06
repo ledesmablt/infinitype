69d37f984e99edd447048011ec003ec3c4cf2e0f
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-06 18:45:50 +0800

    added footer link and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,14 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="Footer">
-      Footer
+      <a
+        className="link-fade-in"
+        href="https://github.com/ledesmablt/infinitype"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
     </footer>
   )
 }
