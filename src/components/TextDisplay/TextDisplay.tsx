d6fcda4ccfd7cd15ed7debb0681682d1082a26c7
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-07 19:42:54 +0800

    log valid keypresses

--- src/components/TextDisplay/TextDisplay.tsx
+++ src/components/TextDisplay/TextDisplay.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 import './TextDisplay.css';
 
+const defaultValidChars = /^([ A-Za-z0-9_@./#&+-]|Backspace|Tab)$/
+
 function TextDisplay() {
   const text = 'the quick brown fox jumps over the lazy dog '
     .repeat(8).trim();
 
+  function trackKeyPress(event: React.KeyboardEvent): void {
+    if (!RegExp(defaultValidChars).test(event.key)) {
+      return
+    }
+    console.log(event.key);
+  }
+
   const wordBlocks = text.split(' ').map(word => (
-    <div className="Word">{
+    <div
+      className="Word"
+      tabIndex={0}
+      onKeyDown={trackKeyPress}
+    >{
       word.split('').map(letter => (
         <div className="Letter">{letter}</div>
       ))
