24035f368871a140b390d780d0fa97396b4b98ef
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-10 10:36:00 +0800

    moved provider to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import store from './store';
 import './App.css';
 import { changeTheme } from './utils/themes';
 
@@ -16,11 +14,9 @@ function App() {
 
   return (
     <div className="App">
-      <Provider store={store}>
-        <Header />
-        <Main />
-        <Footer />
-      </Provider>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
