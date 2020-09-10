ebc46ecba50690f702aad1002037c8b8833e8a70
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-10 10:48:24 +0800

    added RootReducer store type; load default store value for theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
+import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './App.css';
 import { changeTheme } from './utils/themes';
+import { RootState } from './types/storeTypes';
 
 function App() {
+  const theme = useSelector((state: RootState) => state.theme.name);
+
   useEffect(() => {
     // ideally this would be done in the store
-    changeTheme('dracula');
-  }, [])
+    changeTheme(theme);
+  }, [theme])
 
   return (
     <div className="App">
