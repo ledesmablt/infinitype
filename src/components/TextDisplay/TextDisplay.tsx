526737f434460ce06d3ecfe37157303d7afcf94d
Benj Ledesma <benj.ledesma@gmail.com>
2020-09-10 23:07:46 +0800

    get stylign for text accuracy from store

--- src/components/TextDisplay/TextDisplay.tsx
+++ src/components/TextDisplay/TextDisplay.tsx
@@ -20,6 +20,9 @@ function TextDisplay() {
   const currentWordBank = useSelector(
     (state: RootState) => state.typedChars.wordBank
   );
+  const currentAccuracy = useSelector(
+    (state: RootState) => state.typedChars.charAccuracy
+  );
 
   function trackKeyPress(event: React.KeyboardEvent): void {
     if (!RegExp(defaultValidChars).test(event.key)) {
@@ -37,6 +40,7 @@ function TextDisplay() {
     dispatch({ type: 'UPDATE_ACCURACY' });
   }
 
+  // render elements
   const typedTextBlocks = currentTypedText.split(' ');
   const wordBlocks = currentWordBank.split(' ').map((targetWord, _wi) => {
     const typedWord = (typedTextBlocks[_wi] || '');
@@ -52,20 +56,24 @@ function TextDisplay() {
         ref={_wi === 0 ? focusRef : null}
       >{
         longestWordArray.map((_li) => {
+          const charAccuracy = ((currentAccuracy || [])[_wi] || [])[_li];
+          let letterClass = 'Letter';
+          switch (charAccuracy) {
+            case 'CORRECT': {
+              letterClass += ' correct';
+              break;
+            }
+            case 'WRONG': {
+              letterClass += ' wrong';
+              break;
+            }
+            case 'OVER': {
+              letterClass += ' wrong-overtype'
+              break;
+            }
+          }
           const typedChar = typedWord.charAt(_li);
           const targetChar = targetWord.charAt(_li);
-          let letterClass = 'Letter';
-          if (typedChar === targetChar) {
-            letterClass += ' correct';
-          } else if (
-            typedChar !== targetChar &&
-            typedChar.length > 0 &&
-            _li < targetWord.length
-          ) {
-            letterClass += ' wrong';
-          } else if (_li >= targetWord.length) {
-            letterClass += ' wrong-overtype';
-          }
 
           return (
             <div
